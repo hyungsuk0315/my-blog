91c823acc9315526b9cecf52948423a2fea86d26
Hyungsuk.kim <Hyungsuk.kim@cmes.kr>
2022-01-13 13:44:33 +0900

    add auth button on tool bar

---
+++ src/components/auth/AuthBtn.vue
@@ -0,0 +1,28 @@
+<script lang='ts'>
+import { initializeApp } from 'firebase/app'
+import firebaseConfig from '../../../firebaseConfig.js'
+import {
+  getAuth,
+  GoogleAuthProvider,
+  connectAuthEmulator,
+  signInWithPopup
+} from 'firebase/auth'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AuthBtn',
+  setup () {
+    console.log('sdfasf')
+    initializeApp(firebaseConfig)
+    const auth = getAuth()
+    connectAuthEmulator(auth, 'http://localhost:9099')
+    auth.useDeviceLanguage()
+    const provider = new GoogleAuthProvider()
+    return { auth, provider, signInWithPopup }
+  }
+})
+</script>
+
+<template>
+  <button>sdjfalskf</button>
+</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AuthBtn from '../components/auth/AuthBtn.vue' // eslint-disable-line no-unused-vars
+</script>
+<template>
+  <q-layout
+    view = 'hhh lpr fff'
+    :container='false'
+  >
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-title>
+          my-blog
+        </q-toolbar-title>
+          <AuthBtn />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
